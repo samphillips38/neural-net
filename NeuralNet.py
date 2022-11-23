eadbadcfdb1775c7d75eb30071e42ce71221757d
Sam Phillips <srp60@cam.ac.uk>
2022-11-23 09:11:16 +0000

    Added initial derivative code

--- NeuralNet.py
+++ NeuralNet.py
@@ -18,35 +18,66 @@ class NeuralNet:
 
     def addData(self, XTrain, YTrain):
         """XTrain[i, j]: Dimension j of training sample i"""
-        assert(XTrain[0].shape == self.Nn[0], "Training data input dimension does not match input layer")
-        assert(YTrain[0].shape == self.Nn[-1], "Training data output dimension does not match output layer")
+        assert XTrain.shape[1] == self.Nn[0], "Training data input dimension does not match input layer"
+        assert YTrain.shape[1] == self.Nn[-1], "Training data output dimension does not match output layer"
         self.XTrain = XTrain
         self.YTrain = YTrain
         self.D = self.XTrain.shape[1]
 
-    def relu(x):
+    def relu(self, x):
         return np.maximum(np.zeros(x.shape), x)
 
-    def softmax(x):
+    def softmax(self, x):
         return np.exp(x) / np.sum(np.exp(x))
 
-    def _y(self, i, input):
+    def z(self, l, x):
+        """z for layer l"""
+        assert l >= 0 and l < self.L, f"Layer {i} is not valid"
+        if l == 0:
+            return x
+        return self.W @ self.y(l-1, x)
+
+    def y(self, l, x):
         """Output y of layer i"""
-        assert(i >= 0 and i < self.L-1, "Layer {i} is not valid")
-        if i == 0:
-            return input
-        return self.relu( self.W @ self._y(i-1, input) )
+        assert l >= 0 and l < self.L, f"Layer {l} is not valid"
+        if l == 0:
+            return x
+        if l == self.L-1:
+            return self.softmax( self.z(l) )
+        return self.relu( self.z(l) )
     
-    def predict(self, input):
-        return self.softmax( self.W @ self._y(self.L-2, input) )
+    def predict(self, x):
+        return self.y(self.L-1, x)
         
     def input_cost(self, x, y):
-        np.linalg.norm(self.predict(x) - y, ord=2)
+        return np.linalg.norm(self.predict(x) - y, ord=2)
 
     def C(self):
         s = 0
         for i in range(self.D):
             s += self.input_cost(self.XTrain[i], self.YTrain[i])
         return s
+    
+    def drelu(self, x):
+        pass
 
+    def dsoftmax(self, x):
+        return self.softmax(x) * (1 - self.softmax(x))
 
+    def dydw(self, li, lj, x):
+        """Derivative of y at layer li by w at layer lj"""
+        assert lj <= li, "Cannot find forward derivative"
+        if li == self.L-1:
+            return self.dsoftmax( self.z(li-1, x) ) @ self.dzdw(li, lj, x)
+        return self.drelu( self.z(li-1, x) ) @ self.dzdw(li, lj, x)
+
+    def dzdw(self, li, lj, x):
+        """Derivative of z at layer li by w at layer lj"""
+        assert lj < li, "Cannot find forward derivative"
+        if li-1 == lj:
+            return self.y(lj, x)
+        return self.W[li-1] @ self.dydw(li-1, lj, x)
+
+    def dCdw(self, x, y, l):
+        """Derivative wrt w at layer l"""
+        return 2 * (self.predict(x) - y) @ self.dydw(self.L-1, l)
--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-from .NeuralNet import NeuralNet
+from NeuralNet import NeuralNet
+import numpy as np
 
+
+
+NN = NeuralNet(3, [3, 5, 3])
+XTrain = np.array([
+    [1, 2, 3],
+    [1, 3, 2],
+    [4, 2, 3],
+    [5, 1, 1],
+    [-5, -3, -2],
+    [-2, -2, -3],
+    [-3, -1, -2]
+])
+YTrain = np.array([
+    [1, 0, 0],
+    [1, 0, 0],
+    [0, 0, 1],
+    [0, 0, 1],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0]
+])
+input = np.array([[100, 2, 3]]).T
+
+NN.addData(XTrain, YTrain)
+print(NN.C())
